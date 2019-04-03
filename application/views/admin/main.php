022eb1d94dcd72f80d1ad9b415aff4287284b6d7
Yugi <yugi13071997@gmail.com>
2019-04-03 19:15:24 +0700

    Update Yugi

--- application/views/admin/main.php
+++ application/views/admin/main.php
@@ -2,7 +2,7 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
     <title>SIPPS | Admin</title>
 
     <!-- Favicon icon -->
